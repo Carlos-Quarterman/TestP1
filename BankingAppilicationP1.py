42c4ec3dcb0b801334c09158de2ac1cbdbd53505
Carlos-Quarterman <carlos.quarterman@outlook.com>
2022-05-04 14:06:15 -0400

    Initial commit, added project

--- BankingAppilicationP1.py
+++ BankingAppilicationP1.py
@@ -1,12 +1,17 @@
 from ast import And, For
+import collections
 import select
 from unittest import result
 import pymongo
 from pymongo import mongo_client
+import json
 
-client = pymongo.MongoClient("127.0.0.1", 27017) 
-db = client.Project1
-collection = db.account
+myclient = pymongo.MongoClient("127.0.0.1", 27017) 
+db = myclient.Project1
+collections = myclient.account
+
+with open('account.json') as a1:
+  file_data = json.load(a1)
 
 class BankingAppilication :
 
@@ -14,76 +19,95 @@ class BankingAppilication :
  LastName = ""
  AccountNumC = ""
  AccountNumS = ""
- BalanceC1 = ""
- BalanceS1 = ""
+ BalanceC = ""
+ BalanceS = ""
  
+# Update Balance
+def Deposit():
+ amount = input("Enter amount to be deposit")
+ print("Amount Desposit: ", amount)
+ file_data.BalanceC = file_data.BalanceC + amount
+print("Balance: ", file_data.BalanceC)
+
+
+#delete 
+def Withdraw():
+ amount = input("Enter amount to be deposit")
+ print("Amount Desposit: ", amount)
+ file_data.BalanceC = file_data.BalanceC - amount
+print("Balance: ", file_data.BalanceC)
+
+
+#Reading Account
 def Checking():
- AccountNumC = int(input("Enter Checking Account Number: "))
- collection.find({AccountNumC : ""}) 
- if AccountNumC == 5475835230 :
-  print("First Name: " 'Tom')
-  print("Last Name: " 'Jones')
-  print("Checking Account Number:" , AccountNumC)
-  print("Balance:" " ${:,.2f}".format(1900.86))
- elif AccountNumC == 5475806426 :
-  print("First Name: " 'Erica')
-  print("Last Name: " 'Johnson')
-  print("Checking Account Number:" , AccountNumC)
-  print("Balance:" " ${:,.2f}".format(3940.86))
- elif AccountNumC == 5475835453 :
-  print("First Name: " 'Kim')
-  print("Last Name: " 'Thomas')
-  print("Account Number:" , AccountNumC)
-  print("Balance:" " ${:,.2f}".format(6764.32))
- else :
-  print("Invalid")
-def Saving():
- AccountNumS = int(input("Enter Saving Account Number: "))
- collection.find({AccountNumS : ""}) 
- if AccountNumS == 5475835231 :
-  print("First Name: " 'Tom')
-  print("Last Name: " 'Jones')
-  print("Saving Account Number:" , AccountNumS)
-  print("Balance:" " $ {:,.2f}".format(500876.89))
- elif AccountNumS == 5475806427 :
-  print("First Name: " 'Erica')
-  print("Last Name: " 'Johnson')
-  print("Saving Account Number:", AccountNumS)
-  print("Balance:" " ${:,.2f}".format(4087.89))
- elif AccountNumS == 5475835454 :
-  print("First Name: " 'Kim')
-  print("Last Name: " 'Thomas')
-  print("Saving Account Number:", AccountNumS)
-  print("Balance:" " $ {:,.2f}".format(90343.04))
- else :
-  print("Invalid")
+  AccountNumC = int(input("Enter Checking Account Number: "))
+  file_data.collection.find(AccountNumC) 
+  if AccountNumC == file_data.collection.find() :
+   print(file_data.FirstName)
+   print(file_data.LastName)
+   print("Checking Account Number:" , AccountNumC)
+   print("Balance:" " ${:,.2f}".format(file_data.BalanceC))
+ #elif AccountNumC == 5475806426 :
+  #print("First Name: " 'Erica')
+  #print("Last Name: " 'Johnson')
+  #print("Checking Account Number:" , AccountNumC)
+  #print("Balance:" " ${:,.2f}".format(3940.86))
+ #elif AccountNumC == 5475835453 :
+  #print("First Name: " 'Kim')
+  #print("Last Name: " 'Thomas')
+  #print("Account Number:" , AccountNumC)
+  #print("Balance:" " ${:,.2f}".format(6764.32))
+  else :
+   print("Invalid")
+#def Saving():
+ #AccountNumS = int(input("Enter Saving Account Number: "))
+ #file_data.collection.find({AccountNumS : ""}) 
+ #if AccountNumS == 5475835231 :
+  #print("First Name: " 'Tom')
+  #print("Last Name: " 'Jones')
+  #print("Saving Account Number:" , AccountNumS)
+  #print("Balance:" " $ {:,.2f}".format(500876.89))
+ #elif AccountNumS == 5475806427 :
+  #print("First Name: " 'Erica')
+  #print("Last Name: " 'Johnson')
+  #print("Saving Account Number:", AccountNumS)
+  #print("Balance:" " ${:,.2f}".format(4087.89))
+ #elif AccountNumS == 5475835454 :
+  #print("First Name: " 'Kim')
+  #print("Last Name: " 'Thomas')
+  #print("Saving Account Number:", AccountNumS)
+  #print("Balance:" " $ {:,.2f}".format(90343.04))
+ #else :
+  #print("Invalid")
 
 def DisplayAll():
     FirstName = input("Enter First Name: ")
+    file_data.collection.find(FirstName)
     LastName = input("Enter Last Name: ")
-    collection.find({FirstName : ''})
-    collection.find({LastName : ''}) 
-    if FirstName == 'Tom' and LastName == 'Jones' :
+    file_data.collection.find(FirstName)
+   # account.find({FirstName : ''})
+   # account.find({LastName : ''}) 
+    if FirstName and LastName  :
      print(FirstName)
      print(LastName)
-     print("Checking Account Number: " '5475835230')
-     print("Checking Account Balance:" " ${:,.2f}".format(1900.86))
-     print("Saving Account Number: " '5475835231')
-     print("Saving Account Balance:" " ${:,.2f}".format(500876.89))
-    elif FirstName == 'Erica' and LastName == 'Johnson':
+     print("Checking Account Number: ", file_data.collection.find.AccountNumC)
+     print("Checking Account Balance:" " ${:,.2f}".format(file_data.collection.find.BalanceC))
+     print("Saving Account Number: ", file_data.collection.find.BalanceS)
+     print("Saving Account Balance:" " ${:,.2f}".format(file_data.collection.find.BalanceS))
+    elif FirstName  and LastName :
      print(FirstName)
      print(LastName)
-     print("Checking Account Number: " '5475806426')
-     print("Checking Account Balance:" " ${:,.2f}".format(3940.86))
-     print("Saving Account Number: " '5475806427')
-     print("Saving Account Balance:" " ${:,.2f}".format(4087.89))
-    elif FirstName == 'Kim' and LastName == 'Thomas':
+     print("Checking Account Number: ", file_data.collection.find.AccountNumC)
+     print("Checking Account Balance:" " ${:,.2f}".format(file_data.collection.find.BalanceC))
+     print("Saving Account Number: ", file_data.collection.find.AccountNumS)
+     print("Saving Account Balance:" " ${:,.2f}".format(file_data.collection.find.BalanceS))
+    elif FirstName and LastName :
      print(FirstName)
      print(LastName)
-     print("Checking Account Number: " '5475835453')
-     print("Checking Account Balance:" " ${:,.2f}".format(6764.32))
-     print("Saving Account Number: " '5475835454')
-     print("Saving Account Balance:" " ${:,.2f}".format(90343.04))
+     print("Checking Account Number: ", file_data.collection.find.AccountNumC)
+     print("Checking Account Balance:" " ${:,.2f}".format(file_data.collection.find.BalanceC))
+     print("Saving Account Number: ", file_data.collection.find.AccountNumS)
+     print("Saving Account Balance:" " ${:,.2f}".format(file_data.collection.find.BalanceS))
     else :
        print("Invalid")
 
@@ -107,19 +131,23 @@ num = 0
 
 Intro()
 
-while BC != 4:
+while BC != 5:
 
    print("\tMain Menu")
 
    print("\t1. Checking Account")
 
-   print("\t2. Savings Account")
+   #print("\t2. Savings Account")
 
-   print("\t3. Display Account Information")
+   print("\t2. Deposit")
 
-   print("\t4. Exit")
+   print("\t3. Withdown")
 
-   print("\tSelect Your Option (1-4): ")
+   print("\t4. Display Account Information")
+
+   print("\t5. Exit")
+
+   print("\tSelect Your Option (1-6): ")
 
    BC = input()
 
@@ -127,15 +155,23 @@ while BC != 4:
 
      Checking()
      
-   elif BC =='2':
+   #elif BC =='2':
 
-     Saving()
+     #Saving()
+
+   elif BC == '2':
+
+     Deposit()
 
    elif BC == '3':
 
-     DisplayAll()
+     Withdraw()
 
    elif BC == '4':
+
+     DisplayAll()
+
+   elif BC == '5':
 
        print("\tThanks for using Apple Banking System")
 
